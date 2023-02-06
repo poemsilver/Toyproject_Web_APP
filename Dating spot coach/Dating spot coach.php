60074b5a1930dc88a28c43f092e7625db9c5d00d
poemsilver <120539836+poemsilver@users.noreply.github.com>
2023-02-06 14:58:27 +0900

    Create Dating spot coach.php

---
+++ Dating spot coach/Dating spot coach.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Korea Dating Places Recommendation</title>
+  </head>
+  <body>
+    <h1 id="title">Recommendations for Dating Places in Korea</h1>
+    <div id="weather-info"></div>
+    <div id="recommendation"></div>
+
+    <script>
+      const weatherInfoDiv = document.querySelector("#weather-info");
+      const recommendationDiv = document.querySelector("#recommendation");
+
+      // sample data for weather information, you would need to fetch the current weather information using a weather API
+      const weather = "clear";
+
+      if (weather === "clear") {
+        weatherInfoDiv.innerHTML = "The weather is clear today. Enjoy the beautiful weather!";
+        recommendationDiv.innerHTML = "Here are some outdoor dating places you might want to check out:<br><br>1. Namsan Tower<br>2. Yeouido Park<br>3. Gyeongbokgung Palace<br>4. Jeju Island<br>5. Seoraksan National Park";
+      } else {
+        weatherInfoDiv.innerHTML = "It's cloudy or raining today. How about trying an indoor dating spot?";
+        recommendationDiv.innerHTML = "Here are some indoor dating places you might want to check out:<br><br>1. COEX Aquarium<br>2. Trickeye Museum<br>3. National Museum of Korea<br>4. Hongdae Playground<br>5. Lotte World Tower Observatory";
+      }
+    </script>
+  </body>
+</html>
